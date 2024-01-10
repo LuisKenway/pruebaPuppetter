2008210a5a29dd8a5c4ab2782787777572ba1889
PabloLUC9832 <lucaspablo340@outlook.com>
2024-01-09 21:24:28 -0600

    ajustes

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM ghcr.io/puppeteer/puppeteer:latest
+
+COPY /prueba /
--- index.js
+++ index.js
@@ -5,46 +5,46 @@ const raes = {
 
     /* RAE INTERNO(oficina todos los clientes) */
     //RAE 1 Maritimo
-    RAE_1_Maritimo_MZO : 'a[href="https://webdev.galvan.com/ion/rae/RAE-M.cfm?A%3E%29%25%3BMX%3A%22%3DM%260N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_1_Maritimo_VER : 'a[href="https://webdev.galvan.com/ion/rae/RAE-M.cfm?A%3E%29%25%3BMX%3A%22%3DM%220N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_1_Maritimo_LZC : 'a[href="https://webdev.galvan.com/ion/rae/RAE-M.cfm?A%3E%29%25%3BMX%3A%22%3DM%5E0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_1_Maritimo_ALT: 'a[href="https://webdev.galvan.com/ion/rae/RAE-M.cfm?B%3E%29%25%3BMX%3A%22%3DM%3A%26%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A"]',
+    RAE_1_Maritimo_MZO : 'https://webdev.galvan.com/ion/rae/RAE-M.cfm?A%3E%29%25%3BMX%3A%22%3DM%260N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_1_Maritimo_VER : 'https://webdev.galvan.com/ion/rae/RAE-M.cfm?A%3E%29%25%3BMX%3A%22%3DM%220N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_1_Maritimo_LZC : 'https://webdev.galvan.com/ion/rae/RAE-M.cfm?A%3E%29%25%3BMX%3A%22%3DM%5E0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_1_Maritimo_ALT: 'https://webdev.galvan.com/ion/rae/RAE-M.cfm?B%3E%29%25%3BMX%3A%22%3DM%3A%26%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A',
     //RAE 2 Aereo *NUEVO*
-    RAE_2_Aereo_NUEVO_MEX : 'a[href="https://webdev.galvan.com/ion/rae/RAE-M-A.cfm?A%3E%29%25%3BMX%3A%22%3DM20N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_2_Aereo_NUEVO_GDL : 'a[href="https://webdev.galvan.com/ion/rae/RAE-M-A.cfm?A%3E%29%25%3BMX%3A%22%3DM%2E0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_2_Aereo_NUEVO_QRO : 'a[href="https://webdev.galvan.com/ion/rae/RAE-M-A.cfm?B%3E%29%25%3BMX%3A%22%3DM6%27%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A"]',
+    RAE_2_Aereo_NUEVO_MEX : 'https://webdev.galvan.com/ion/rae/RAE-M-A.cfm?A%3E%29%25%3BMX%3A%22%3DM20N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_2_Aereo_NUEVO_GDL : 'https://webdev.galvan.com/ion/rae/RAE-M-A.cfm?A%3E%29%25%3BMX%3A%22%3DM%2E0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_2_Aereo_NUEVO_QRO : 'https://webdev.galvan.com/ion/rae/RAE-M-A.cfm?B%3E%29%25%3BMX%3A%22%3DM6%27%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A',
 
     //RAE Maritimo/Aereo (HP)
-    RAE_Maritimo_Aereo_HP_TODOS:'a[href="https://webdev.galvan.com/ion/rae/RAE-HP-A_M.cfm?office=%22%2EL8%20%0A&email=%233I%29%2E%0A&PrintEmail=%233I%29%2E%0A"]',
+    RAE_Maritimo_Aereo_HP_TODOS:'https://webdev.galvan.com/ion/rae/RAE-HP-A_M.cfm?office=%22%2EL8%20%0A&email=%233I%29%2E%0A&PrintEmail=%233I%29%2E%0A',
     //RAE 5 Op. Especiales
-    RAE_5_Op_Especiales : 'a[href="https://webdev.galvan.com/ion/rae/RAE-OpeEsp.cfm?B%3E%29%25%3BMX%3A%22%3DLJ%27%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A"]',
+    RAE_5_Op_Especiales : 'https://webdev.galvan.com/ion/rae/RAE-OpeEsp.cfm?B%3E%29%25%3BMX%3A%22%3DLJ%27%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A',
 
     /* RAE PERSONALIZADOS */
     // RAE Laredo (HP)
-    RAE_Laredo_HP : 'a[href="https://webdev.galvan.com/ion/rae/RAE-HP.cfm?A%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]' ,
+    RAE_Laredo_HP : 'https://webdev.galvan.com/ion/rae/RAE-HP.cfm?A%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A' ,
     // RAE Laredo (GGI - RAMSA)
     RAE_Laredo_GGI_RAMSA_GGI : 'https://webdev.galvan.com/ion/rae/RAE_3_3.cfm?M%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2DNO%21%22A%5CJRBI%2FB%28%20%3F%5E%0A%21C0%20%20%0A' ,
     RAE_Laredo_GGI_RAMSA_MEX : 'https://webdev.galvan.com/ion/rae/RAE_2_2.cfm?M%3E%29%25%3BMX%3A%22%3DM20N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2DNO%21%22A%5CJRBI%2FB%28%20%3F%5E%0A%21C0%20%20%0A' ,
     // RAE Laredo/NLaredo (INVISTA)
-    RAE_Laredo_NLaredo_INVISTA_1 : 'a[href="https://webdev.galvan.com/ion/rae/RAE_3_2.cfm?A%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]' ,
-    RAE_Laredo_NLaredo_INVISTA_2: 'a[href="https://webdev.galvan.com/ion/rae/RAE_4_2.cfm?A%3E%29%25%3BMX%3A%22%3DM60N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]' ,
+    RAE_Laredo_NLaredo_INVISTA_1 : 'https://webdev.galvan.com/ion/rae/RAE_3_2.cfm?A%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A' ,
+    RAE_Laredo_NLaredo_INVISTA_2: 'https://webdev.galvan.com/ion/rae/RAE_4_2.cfm?A%3E%29%25%3BMX%3A%22%3DM60N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A' ,
 
     //RAE 3.4 Kuehne Nagel LTX
-    RAE_3_4_Kuehne_Nagel_LTX : 'a[href="https://webdev.galvan.com/ion/rae/RAE_3_4.cfm?A%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]' ,
+    RAE_3_4_Kuehne_Nagel_LTX : 'https://webdev.galvan.com/ion/rae/RAE_3_4.cfm?A%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A' ,
     //RAE 5.1 Ope Esp (HP)
-    RAE_5_1_Ope_Esp_HP : 'a[href="https://webdev.galvan.com/ion/rae/RAE_5_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%2A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]' ,
+    RAE_5_1_Ope_Esp_HP : 'https://webdev.galvan.com/ion/rae/RAE_5_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%2A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A' ,
 
     /* RAE EXTERNO */
     /*RAE 6.1 clientes*/
-    RAE_6_1_clientes_MEX:'a[href="https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM20N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_6_1_clientes_GDL:'a[href="https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%2E0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_6_1_clientes_MZO:'a[href="https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%260N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_6_1_clientes_VER: 'a[href="https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%220N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_6_1_clientes_LZC:'a[href="https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%5E0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
-    RAE_6_1_clientes_ALT : 'a[href="https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?B%3E%29%25%3BMX%3A%22%3DM%3A%26%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A"]',
-    RAE_6_1_clientes_QRO:'a[href="https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?B%3E%29%25%3BMX%3A%22%3DM6%27%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A"]',
+    RAE_6_1_clientes_MEX:'https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM20N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_6_1_clientes_GDL:'https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%2E0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_6_1_clientes_MZO:'https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%260N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_6_1_clientes_VER: 'https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%220N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_6_1_clientes_LZC:'https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?A%3E%29%25%3BMX%3A%22%3DM%5E0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
+    RAE_6_1_clientes_ALT : 'https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?B%3E%29%25%3BMX%3A%22%3DM%3A%26%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A',
+    RAE_6_1_clientes_QRO:'https://webdev.galvan.com/ion/rae/RAE_6_1.cfm?B%3E%29%25%3BMX%3A%22%3DM6%27%5EK%2E%22%3F%3F%2BPXF%29%3FC%5FT%23%405H9%24K%26%3A7%5F%3E%3AH%40%21%3B%5BP%20%20%0A',
     /* RAE 6.3 Clientes - Todos */
-    RAE_6_3_Clientes_TODOS:'a[href="https://webdev.galvan.com/ion/rae/RAE_6_3.cfm?A%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A"]',
+    RAE_6_3_Clientes_TODOS:'https://webdev.galvan.com/ion/rae/RAE_6_3.cfm?A%3E%29%25%3BMX%3A%22%3DM%3A0N%3BN%2E%3D%3F%3EAAEY%29VHLAFET%23%28YF65%5F%2B%2BQCQ%2D%0A',
 };
 /*
 const raesLargos = {
@@ -65,12 +65,12 @@ async function test() {
     });
 
     const page = await browser.newPage();
-    await page.goto('https://webdev.galvan.com/ion/rae/menu_rae.cfm');
+    //await page.goto('https://webdev.galvan.com/ion/rae/menu_rae.cfm');
 
     for(const rae in raes){
-        console.log(`${rae}: ${raes[rae]}`);
+        //console.log(`${rae}: ${raes[rae]}`);
         let reporte = `${rae}`;
-        await page.click(raes[rae],{
+        await page.goto(raes[rae],{
             waitUntil: "load",
         })
 
@@ -81,6 +81,7 @@ async function test() {
         if(result.length > 0){
             console.log('Error al enviar: ' + reporte );
             console.log('*******\n');
+            await page.screenshot({path: 'Error-'+reporte+'.png'});
         }else{
             console.log(reporte + ' ha sido enviado.');
             console.log('-------\n');
